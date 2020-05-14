0a81c8439faeeed20f0af439a48dbcc9b8bc50c9
Vinicius Cruz <wh0adscrypt@gmail.com>
2020-05-14 16:05:40 -0300

    feat: Add Leads endpoint

---
+++ KivalitaAPI/Models/Leads.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+using KivalitaAPI.Interfaces;
+
+namespace KivalitaAPI.Models {
+	public class Leads : IEntity {
+		[JsonIgnore]
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Position { get; set; }
+		public string Email { get; set; }
+		public string Company { get; set; }
+		public string Sector { get; set; }
+		public string CompanySite { get; set; }
+		public string CompanyLinkedIn { get; set; }
+		public string Phone { get; set; }
+		public string LinkedIn { get; set; }
+	}
+}
