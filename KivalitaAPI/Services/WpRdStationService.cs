b443b8b3f2f369a0d5100216ff8153e23d47eb2d
Romulo Tone <rootcarvalho@gmail.com>
2020-06-29 11:11:30 -0300

    Adicionando requisição do formulário de Newsletter

--- KivalitaAPI/Services/WpRdStationService.cs
+++ KivalitaAPI/Services/WpRdStationService.cs
@@ -2,6 +2,7 @@
 using KivalitaAPI.Data;
 using KivalitaAPI.Models;
 using KivalitaAPI.Repositories;
+using System;
 
 namespace KivalitaAPI.Services
 {
@@ -9,6 +10,12 @@ namespace KivalitaAPI.Services
     public class WpRdStationService : Service<WpRdStation, KivalitaApiContext, WpRdStationRepository>
     {
         public WpRdStationService(KivalitaApiContext context, WpRdStationRepository baseRepository) : base(context, baseRepository) { }
+
+        public override WpRdStation Add(WpRdStation integration)
+        {
+            integration.CreatedAt = DateTime.UtcNow;
+            return base.Add(integration);
+        }
     }
 }
 
