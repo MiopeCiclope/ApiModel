b87eba6ff6284b0831c36fa04388c1e4465210fe
Douglas Laiber <douglaslbosco@gmail.com>
2020-07-16 15:37:10 -0300

    FlowTask repository

---
+++ KivalitaAPI/Repositories/FlowTaskRepository.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using KivalitaAPI.Models;
+using Sieve.Services;
+
+namespace KivalitaAPI.Repositories
+{
+    public class FlowTaskRepository : Repository<FlowTask, DbContext, SieveProcessor>
+    {
+        public FlowTaskRepository(DbContext context, SieveProcessor filterProcessor) : base(context, filterProcessor) { }
+    }
+}
+
