7bb3890a177f1459241a0a3f5e98901a97edbb8e
Romulo Tone <rootcarvalho@gmail.com>
2020-08-24 11:51:40 -0300

    Rotina de inclusão de Tags

---
+++ KivalitaAPI/Repositories/LeadTagRepository.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using KivalitaAPI.Models;
+using Sieve.Services;
+
+namespace KivalitaAPI.Repositories
+{
+    public class LeadTagRepository : Repository<LeadTag, DbContext, SieveProcessor>
+    {
+        public LeadTagRepository(DbContext context, SieveProcessor filterProcessor) : base(context, filterProcessor) { }
+    }
+}
+
