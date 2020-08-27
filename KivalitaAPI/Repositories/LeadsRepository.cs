55e4f49834dd2ca77b901e301eeead54533d19bc
Romulo Tone <rootcarvalho@gmail.com>
2020-08-27 12:00:20 -0300

    Ajuste na consulta de usuário sem a assinatura

--- KivalitaAPI/Repositories/LeadsRepository.cs
+++ KivalitaAPI/Repositories/LeadsRepository.cs
@@ -164,41 +164,40 @@ namespace KivalitaAPI.Repositories
                                     .ThenInclude(l => l.Tag)
                                 .WithTranslations()
                                 .Where(lead => lead.Deleted == false)
-                                .AsNoTracking()
-                                .Select(l => new Leads
-                                {
-                                    Id = l.Id,
-                                    Name = l.Name,
-                                    Position = l.Position,
-                                    Email = l.Email,
-                                    LinkedIn = l.LinkedIn,
-                                    Status = l.Status,
-                                    Flow = l.Flow,
-                                    FlowId = l.FlowId,
-                                    CreatedAt = l.CreatedAt,
-                                    LeadTag = l.LeadTag,
-                                    CompanyId = l.CompanyId,
-                                    Company = new Company
-                                    {
-                                        Id = l.Company.Id,
-                                        Name = l.Company.Name,
-                                        Sector = l.Company.Sector,
-                                        UserId = l.Company.UserId,
-                                        User = new User
-                                        {
-                                            Id = l.Company.User.Id,
-                                            FirstName = l.Company.User.FirstName,
-                                            LastName = l.Company.User.LastName
-                                        }
-                                    }
-
-                                });
+                                .AsNoTracking();
 
             result = this.filterProcessor.Apply(filterQuery, result, applyPagination: false).WithTranslations();
             var total = result.Count();
 
             result = this.filterProcessor.Apply(filterQuery, result, applyFiltering: false, applySorting: false);
-            var list = result.ToList();
+            var list = result.Select(l => new Leads
+            {
+                Id = l.Id,
+                Name = l.Name,
+                Position = l.Position,
+                Email = l.Email,
+                LinkedIn = l.LinkedIn,
+                Status = l.Status,
+                Flow = l.Flow,
+                FlowId = l.FlowId,
+                CreatedAt = l.CreatedAt,
+                LeadTag = l.LeadTag,
+                CompanyId = l.CompanyId,
+                Company = new Company
+                {
+                    Id = l.Company.Id,
+                    Name = l.Company.Name,
+                    Sector = l.Company.Sector,
+                    UserId = l.Company.UserId,
+                    User = new User
+                    {
+                        Id = l.Company.User.Id,
+                        FirstName = l.Company.User.FirstName,
+                        LastName = l.Company.User.LastName
+                    }
+                }
+
+            }).ToList();
 
             return new QueryResult<Leads>
             {
