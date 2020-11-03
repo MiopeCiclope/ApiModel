eac238e69aed911aa3d4e1b02fc53e1a1ff5d36b
Romulo Tone <rootcarvalho@gmail.com>
2020-11-03 10:03:47 -0300

    Retornando Schema certo

--- KivalitaAPI/Setting.cs
+++ KivalitaAPI/Setting.cs
@@ -5,6 +5,6 @@ namespace KivalitaAPI
     {
         public static string Secret = "7ece221bf3f5dbddbe3c2770ac19b419";
         public static string MailTrackSecret = "149afd631693c895f81e508eb5aaef37";
-        public static string Schema = "dbo";
+        public static string Schema = "14435_admin";
     }
 }
